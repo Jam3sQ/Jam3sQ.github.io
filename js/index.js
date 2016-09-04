f6aac74a7f4e301d4d430c5b2f5c985e71bba70b
James Quiambao <james.quiambao@yahoo.ca>
2016-09-03 23:25:36 -0400

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,13 @@
+var htmlAndBody = $('html,body');
+
+var homeOffsetTop = $("#Profile-Section").offset().top;
+var aboutOffsetTop = $("#About-Section").offset().top;
+var projectsOffsetTop = $("#Projects-Section").offset().top;
+var contactOffsetTop = $("#Contact-Section").offset().top;
+
+function smoothScrollTo(offsetTop) {
+  event.preventDefault();
+  htmlAndBody.animate({
+    scrollTop: offsetTop
+  }, 1000);
+}
